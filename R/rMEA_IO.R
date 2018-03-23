b7e1f5e6989c4129623a772e363db8b990bf0c65
Johann R. Kleinbub <johann.kleinbub@gmail.com>
2018-03-23 15:40:05 +0100

    NA compatible readMEA

--- R/rMEA_IO.R
+++ R/rMEA_IO.R
@@ -195,7 +195,7 @@ readMEA = function(
   #check if any MEA is > 10sd
   Map(function(x,i){
     myDat = c(x$s1,x$s2)
-    mySD = stats::sd(myDat)
+    mySD = stats::sd(myDat, na.rm = T)
     if(any(myDat > 10*mySD))
       warning( round(length(myDat[myDat > 10*mySD])/length(myDat)*100,2) ,"% of the data was higher than 10 standard deviations in dyad: ",dyadIds[[i]], ", session: ",sess[[i]],", group:",group[[i]], ". Check the raw data!", call. = F)
   },lf,seq_along(lf))
