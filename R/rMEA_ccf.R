4c461f6d3c0f1366868d26d8962d9b6570c988e8
kleinbub <johann.kleinbub@gmail.com>
2022-09-20 11:58:26 +0200

    cleanup

--- R/rMEA_ccf.R
+++ R/rMEA_ccf.R
@@ -143,21 +143,16 @@ MEAccf.MEA = function(mea, lagSec, winSec, incSec, r2Z=T, ABS=T){
   if(r2Z) ccfmat = fisher.r2z(ccfmat)
   if(ABS) ccfmat = abs(ccfmat)
 
-<<<<<<< HEAD
-
   startx = (seq_len(n_win)-1) * incSec
   endx   = (seq_len(n_win)-1) * incSec + winSec
   if(any(startx%%1 !=0) || any(endx%%1 !=0) ){
+    #if there are some fractional windows do not convert to mm:ss format
 
   } else { #if all are integers
     startx = timeMaster((seq_len(n_win)-1) * incSec, out="h")
     endx   = timeMaster((seq_len(n_win)-1) * incSec + winSec, out="h")
   }
 
-=======
-  startx = timeMaster(round((seq_len(n_win)-1) * incSec), out="h")
-  endx   = timeMaster(round((seq_len(n_win)-1) * incSec + winSec), out="h")
->>>>>>> 6b09454e3da3502044d99d52f54879afa72d5521
   timex = data.frame(start=startx, end=endx)
   rownames(timex) = paste0("w",seq_len(n_win))
 
